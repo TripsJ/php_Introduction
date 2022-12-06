f53ed2a3c90403c3be3075278b3af5e9ad756825
Jérôme Trips <jerome.trips@tutanota.com>
2022-12-06 12:19:57 +0100

    variables and conditions

---
+++ src/public/variables/variabletypes.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+<body>
+<p><?php
+$name = "John";
+$age = 36;
+$eyes = "brown";
+$family = ["Sarah", "Olivia", "Luna", "Frederick"];
+echo ($name);
+?></p>
+<p>
+    I am <?php echo($age)?> Years old </p>
+<p> My eyes are <?php echo($eyes)?> </p>
+<p> The first person in my family is: <?php echo($family[1])?>
+</body>
+</html>
+
+
+
+
