b9857cba463f5d5f377d4536f9aa3e8739f0b479
Jérôme Trips <jerome.trips@tutanota.com>
2022-12-07 14:48:26 +0100

    if else

--- src/public/conditionals/conditionsDrill.php
+++ src/public/conditionals/conditionsDrill.php
@@ -21,23 +21,37 @@ else if ($room_filthiness == "clean"){
 	echo "<br> nice",PHP_EOL;
 }
  else {
-	echo "<br>Nothing to do, room is neat.",PHP_EOL;
+	echo "<br>Nothing to do, room is neat.<br>",PHP_EOL;
 }
 
 
 // 2. "Different greetings according to time" Exercise
 date_default_timezone_set('Europe/Berlin');//set timezone
-$now = date('h:m:s',time()); // time() gets time in ms date converts How to get the current time in PHP ? Google is your friend ;-)
+$now = date('H'); //date('h:m:s',time()); // time() gets time in ms date converts How to get the current time in PHP ? Google is your friend ;-)
 
 // Test the value of $now and display the right message according to the specifications.
 // if( condition){
 // }
-echo ($now );
-if ($now < "05:00:00" and $now<"13:00:00"){
-	echo "Good Morning";
-}else{
-	echo "G`Day";
+echo ($now);
+if( $now > 21 and $now < 5)
+{
+	echo "Good night";
 }
-
+else if ($now < 9)
+{
+	echo "Good Morning";
+}
+else if ($now < 12)
+{
+	echo "Good Day";
+}
+else if ($now < 16)
+{
+	echo "Good afternoon";
+}
+else if ($now < 21)
+{
+	echo "Good evening";
+}
 
 ?>
